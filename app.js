344abf27be73a1a0092d2f01ba28862d8ee67b56
Jacob Madden <jacob.madden@gmail.com>
2016-05-07 16:29:02 -0400

    switched from jade to ejs

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var engine       = require('ejs-locals');
 
 var routes = require('./routes/index');
 var api    = require('./routes/api');
@@ -11,8 +12,9 @@ var api    = require('./routes/api');
 var app = express();
 
 // view engine setup
+app.engine( 'ejs', engine );
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
