c58870d6db6a9290c6f54f3b82fd1716bb4e76c2
Jacob Madden <jacob.madden@gmail.com>
2016-05-07 19:02:10 -0400

    fixed incorrect env variable for db

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ var Tone = require('./models/tone');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+mongoose.connect(process.env.MONGODB_URI, function (error) {
   if (error) console.error(error);
   else console.log('mongo connected');
 });
