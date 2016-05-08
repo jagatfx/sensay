5d98cb10dc2110facdc64385329341540dc20ad0
Jacob Madden <jacob.madden@gmail.com>
2016-05-08 07:45:31 -0400

    render consumer by default

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { });
+// });
 
 /* GET consumer page for testing consumer support ticket messages */
-router.get('/consumer', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('consumer', {});
 });
 
