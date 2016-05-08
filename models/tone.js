4d640cfbc8ddfd50e663228be2f3ea8058bee617
Kevin Mulder <kevin.mulder@slalom.com>
2016-05-08 02:25:04 -0400

    More fixes to sentiment value

--- models/tone.js
+++ models/tone.js
@@ -6,7 +6,8 @@ var Tone = new Schema({
   result: Schema.Types.Mixed,
   userName: String,
   userType: String,
-  channel: String
+  channel: String,
+  sentiment: String
 }, {
   timestamps: true
 });
